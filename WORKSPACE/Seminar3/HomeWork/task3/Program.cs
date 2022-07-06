90328e89eacf42324d8dcd19a75ede94c519a3c8
«Nikolay <>
2022-07-06 12:16:36 +0600

    HW-3

---
+++ WORKSPACE/Seminar3/HomeWork/task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 3
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+int Prompt(string message)
+{
+    Console.Write(message);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+
+int n = Prompt("Введите число N: ");
+int count = 1;
+while(count <= n)
+{
+    Console.Write(count * count * count + " ");
+    count++;
+}
