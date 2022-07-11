d4e2f7a2ccc84ed9ce47193fc2415cc2606a32f7
«Nikolay <>
2022-07-11 23:12:58 +0600

    add task3

---
+++ WORKSPACE/Seminar4/task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+int number = Prompt("Введите число: ");
+
+int count = 0;
+while(number > 0)
+{
+    number = number /10;
+    count++;
+}
+System.Console.WriteLine(count);
