994057bf3f1bcc378c43d656320a4a6f2069f2df
«Nikolay <>
2022-07-11 23:21:34 +0600

    add task4

---
+++ WORKSPACE/Seminar4/task4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+int number = Prompt("Введите число: ");
+int mult = 1;
+int i = 1;
+while(i <= number)
+{
+    mult = mult * i;
+    i++;
+}
+System.Console.WriteLine(mult);
