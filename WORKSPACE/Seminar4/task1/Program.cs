2c69a78989ec1bf46626341a1aa103b976b6e544
«Nikolay <>
2022-07-11 22:48:36 +0600

    add task1

---
+++ WORKSPACE/Seminar4/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+int Prompt(string message)
+{
+    Console.Write(message);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+
+int CalcSumDigits(int number)
+{
+int sum = 0;
+while (number >0)
+{
+    sum += number % 10;
+    number = number / 10;
+}
+return sum;
+}
+int number = Prompt("Введите число А: ");
+int result = CalcSumDigits(number);
+System.Console.WriteLine($"Сумма цифр равна {result}");
