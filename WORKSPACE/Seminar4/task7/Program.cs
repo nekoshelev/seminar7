71cc24427bfa6f825f3ff74bc3f796258194873f
«Nikolay <>
2022-07-17 11:17:55 +0600

    add task7

---
+++ WORKSPACE/Seminar4/task7/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Сумма четных цифр числа */
+
+int Promt(string message)
+{
+System.Console.WriteLine(message);
+return int.Parse(Console.ReadLine());
+}
+int number = Promt("Введите число > ");
+
+int sum = 0;
+
+int temp = number;
+
+int count = 1;
+while (temp > 0)
+{
+if ((temp % 10) % 2 == 0) sum = sum + temp % 10;
+temp = temp / 10;
+
+count++;
+
+}
+
+System.Console.WriteLine(sum);
