07118b07590b11d572dff4df8af46bdbfb21bc67
«Nikolay <>
2022-07-17 12:21:38 +0600

    add HW-2

---
+++ WORKSPACE/Seminar4/HomeWork/task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int Promt(string message)
+{
+System.Console.WriteLine(message);
+return int.Parse(Console.ReadLine());
+}
+int n = Promt("Введите число > ");
+int sum = 0;
+
+while (n > 0)
+{
+int number = n % 10;
+n = n / 10;
+sum = sum + number;
+}
+
+System.Console.WriteLine(sum);
