db30ab7fa18fe18ad9f1cf630a7c762a1ab5312b
«Nikolay <>
2022-07-24 12:52:06 +0600

    HW-2

---
+++ WORKSPACE/Seminar5/HomeWork/task2/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 2: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+int[] numbers = new int [4];
+int n = numbers.Length;
+
+FillArray(numbers);
+OutputArrayString(numbers);
+SumStandingElementsOddPositions(numbers);
+
+void FillArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = rnd.Next(-50, 50);
+    }
+}
+
+void OutputArrayString(int[] arrayString)
+{
+    string stringArray = "[";
+
+    for (int i = 0; i < arrayString.Length; i++)
+    {
+        stringArray += $"{arrayString[i]}, ";
+    }
+    int n = stringArray.Length;
+    stringArray = stringArray.Remove(n - 2, 2);
+    stringArray += "]";
+    Console.WriteLine($"{stringArray}");
+}
+
+void SumStandingElementsOddPositions(int[] number)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += number[i];
+        }
+    }
+    Console.WriteLine($"Cумма чисел стоящих на нечётных позициях в массиве равна: {sum}");
+}
---
+++ WORKSPACE/Seminar5/task1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 1: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+// Найдите сумму отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных 
+// чисел равна 29, сумма отрицательных равна -20.
+
+int[] GenerateArray(int length, int min, int max)
+{
+int[] newArray = new int[length];
+Random rnd = new Random();
+for (int i = 0; i < newArray.Length; i++)
+{
+newArray[i] = rnd.Next(min, max+1);
+}
+return newArray;
+}
+
+int CalcSumNegative(int[] array)
+{
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i] < 0)
+sum += array[i];
+}
+return sum;
+}
+
+int CalcSumPositive(int[] array)
+{
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i] > 0)
+sum += array[i];
+}
+return sum;
+}
+
+void PrintArray(int[] array){
+for(int i = 0; i < array.Length; i++){
+System.Console.WriteLine($"{array[i]}; ");
+}
+}
