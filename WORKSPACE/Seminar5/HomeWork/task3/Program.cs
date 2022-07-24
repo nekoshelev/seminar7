531643c35e10c1ccaf19aff5ffc0ed6778b9d8d6
«Nikolay <>
2022-07-24 13:01:37 +0600

    HW-3

---
+++ WORKSPACE/Seminar5/HomeWork/task3/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 3: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+double[] numbers = new double [10];
+int n = numbers.Length;
+
+FillArray(numbers);
+OutputArrayString(numbers);
+DifferenceМinМax(numbers);
+
+void FillArray(double[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = Math.Round(rnd.NextDouble() * 100);
+    }
+}
+
+void DifferenceМinМax(double[] number)
+{
+    double min = number[0];
+    double max = number[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (number[i] > max)
+        {
+            max = number[i];
+        }
+        if (number[i] < min)
+        {
+            min = number[i];
+        }
+    }
+    double diff = max - min;
+    Console.WriteLine($"Разница между максимальным элементом {max} и минимальным элементом {min} массива равна: {diff}");
+}
+
+void OutputArrayString(double[] arrayString)
+{
+    string stringArray = "[";
+
+    for (int i = 0; i < arrayString.Length; i++)
+    {
+        stringArray += $"{arrayString[i]}, ";
+    }
+    int n = stringArray.Length;
+    stringArray = stringArray.Remove(n - 2, 2);
+    stringArray += "]";
+    Console.WriteLine($"{stringArray}");
+}
