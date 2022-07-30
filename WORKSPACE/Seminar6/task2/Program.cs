f94325fd93447b58f9c6b259737c2e0f8c1a3a42
«Nikolay <>
2022-07-30 12:11:31 +0600

    add task2

---
+++ WORKSPACE/Seminar6/task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход
+//три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+int Promt(string message)
+{
+Console.Write (message);
+int a = int.Parse(Console.ReadLine());
+return a;
+}
+
+int A = Promt ( "enter A ");
+int B = Promt ("enter B ");
+int C = Promt ("enter C ");
+
+//int summA = A + B;
+//int summB =
+if (A > B + C || B > A + C || C > B + A)
+{
+Console.WriteLine("такой треугольник не существует");
+}
+else Console.WriteLine(" такой треугольник существует");
